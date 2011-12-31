8fc326be5bab309713d58374e43debc1ce11aff8
Ross Rowe <piaross@gmail.com>
2011-12-31 20:28:49 +1100

    First commit

---
+++ src/main/java/com/saucelabs/MyPlugin.java
@@ -0,0 +1,5 @@
+package com.saucelabs;
+
+public class MyPlugin
+{
+}
---
+++ src/test/java/com/saucelabs/MyPluginTest.java
@@ -0,0 +1,11 @@
+package com.saucelabs;
+
+import org.junit.Test;
+
+public class MyPluginTest
+{
+    @Test
+    public void testSomething()
+    {
+    }
+}
