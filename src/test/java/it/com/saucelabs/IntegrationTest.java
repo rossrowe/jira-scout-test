a489b900dc045927d358fe743e2242516e58ebf1
Ross Rowe <piaross@gmail.com>
2012-02-01 21:07:05 +1100

    Removed firefox driver from code

--- src/test/java/it/com/saucelabs/IntegrationTest.java
+++ src/test/java/it/com/saucelabs/IntegrationTest.java
@@ -2,6 +2,7 @@ package it.com.saucelabs;
 
 import com.saucelabs.ci.sauceconnect.SauceConnectTwoManager;
 import com.saucelabs.rest.Credential;
+import com.saucelabs.selenium.client.factory.SeleniumFactory;
 import org.apache.commons.lang.StringUtils;
 import org.junit.After;
 import org.junit.Before;
@@ -9,9 +10,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.firefox.internal.ProfilesIni;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.Wait;
@@ -21,7 +19,6 @@ import java.io.File;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.Enumeration;
 import java.util.concurrent.TimeUnit;
 
@@ -51,22 +48,22 @@ public class IntegrationTest {
                 credential.saveTo(sauceSettings);
             }
         }
-//        Credential c = new Credential();
-//        sauceTunnelManager = new SauceConnectTwoManager();
-//        Process sauceConnect = (Process) sauceTunnelManager.openConnection(c.getUsername(), c.getKey());
-//        sauceTunnelManager.addTunnelToMap(DUMMY_KEY, sauceConnect);
-//        hostName = getHostName();
-        hostName = "localhost";
+        Credential c = new Credential();
+        sauceTunnelManager = new SauceConnectTwoManager();
+        Process sauceConnect = (Process) sauceTunnelManager.openConnection(c.getUsername(), c.getKey());
+        sauceTunnelManager.addTunnelToMap(DUMMY_KEY, sauceConnect);
+        hostName = getHostName();
+//        hostName = "localhost";
         System.setProperty("SELENIUM_DRIVER", DEFAULT_SAUCE_DRIVER);
         System.setProperty("SELENIUM_PORT", "4445");
         System.setProperty("SELENIUM_HOST", "localhost");
         System.setProperty("SELENIUM_STARTING_URL", "http://" + hostName + ":" + PORT + "/jira/secure/AdminSummary.jspa");
 
-//        driver = SeleniumFactory.createWebDriver();
+        driver = SeleniumFactory.createWebDriver();
 //
-        ProfilesIni allProfiles = new ProfilesIni();
-        FirefoxProfile profile = allProfiles.getProfile("selenium");
-        driver = new FirefoxDriver(profile);
+//        ProfilesIni allProfiles = new ProfilesIni();
+//        FirefoxProfile profile = allProfiles.getProfile("selenium");
+//        driver = new FirefoxDriver(profile);
 
         baseUrl = "http://" + hostName + ":" + PORT + "/jira";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -282,77 +279,9 @@ public class IntegrationTest {
                 fail(verificationErrorString);
             }
         } finally {
-//            sauceTunnelManager.closeTunnelsForPlan(DUMMY_KEY);
+            sauceTunnelManager.closeTunnelsForPlan(DUMMY_KEY);
         }
     }
 
-    /**
-     * Returns an <code>InetAddress</code> object encapsulating what is most likely the machine's LAN IP address.
-     * <p/>
-     * This method is intended for use as a replacement of JDK method <code>InetAddress.getLocalHost</code>, because
-     * that method is ambiguous on Linux systems. Linux systems enumerate the loopback network interface the same
-     * way as regular LAN network interfaces, but the JDK <code>InetAddress.getLocalHost</code> method does not
-     * specify the algorithm used to select the address returned under such circumstances, and will often return the
-     * loopback address, which is not valid for network communication. Details
-     * <a href="http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4665037">here</a>.
-     * <p/>
-     * This method will scan all IP addresses on all network interfaces on the host machine to determine the IP address
-     * most likely to be the machine's LAN address. If the machine has multiple IP addresses, this method will prefer
-     * a site-local IP address (e.g. 192.168.x.x or 10.10.x.x, usually IPv4) if the machine has one (and will return the
-     * first site-local address if the machine has more than one), but if the machine does not hold a site-local
-     * address, this method will return simply the first non-loopback address found (IPv4 or IPv6).
-     * <p/>
-     * If this method cannot find a non-loopback address using this selection algorithm, it will fall back to
-     * calling and returning the result of JDK method <code>InetAddress.getLocalHost</code>.
-     * <p/>
-     *
-     * @throws UnknownHostException If the LAN address of the machine cannot be found.
-     */
-    private static InetAddress getLocalHostLANAddress() throws UnknownHostException {
-        try {
-            InetAddress candidateAddress = null;
-            // Iterate all NICs (network interface cards)...
-            for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements();) {
-                NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
-                // Iterate all IP addresses assigned to each card...
-                for (Enumeration inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements();) {
-                    InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
-                    if (!inetAddr.isLoopbackAddress()) {
-
-                        if (inetAddr.isSiteLocalAddress()) {
-                            // Found non-loopback site-local address. Return it immediately...
-                            return inetAddr;
-                        }
-                        else if (candidateAddress == null) {
-                            // Found non-loopback address, but not necessarily site-local.
-                            // Store it as a candidate to be returned if site-local address is not subsequently found...
-                            candidateAddress = inetAddr;
-                            // Note that we don't repeatedly assign non-loopback non-site-local addresses as candidates,
-                            // only the first. For subsequent iterations, candidate will be non-null.
-                        }
-                    }
-                }
-            }
-            if (candidateAddress != null) {
-                // We did not find a site-local address, but we found some other non-loopback address.
-                // Server might have a non-site-local address assigned to its NIC (or it might be running
-                // IPv6 which deprecates the "site-local" concept).
-                // Return this non-loopback candidate address...
-                return candidateAddress;
-            }
-            // At this point, we did not find a non-loopback address.
-            // Fall back to returning whatever InetAddress.getLocalHost() returns...
-            InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
-            if (jdkSuppliedAddress == null) {
-                throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
-            }
-            return jdkSuppliedAddress;
-        }
-        catch (Exception e) {
-            UnknownHostException unknownHostException = new UnknownHostException("Failed to determine LAN address: " + e);
-            unknownHostException.initCause(e);
-            throw unknownHostException;
-        }
-    }
 
 }
